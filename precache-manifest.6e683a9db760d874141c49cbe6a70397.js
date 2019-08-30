cc01a789e4c213d81316b1cdbc48f4f0ec4cb4ee
mohamdabidi2 <45004605+mohamdabidi2@users.noreply.github.com>
2019-08-30 10:49:06 +0200

    Updates

---
+++ precache-manifest.6e683a9db760d874141c49cbe6a70397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0793444e012b38b414e3908063a48a2a",
+    "url": "/Calculator-with-React/index.html"
+  },
+  {
+    "revision": "2f20050d6602b2fcdccc",
+    "url": "/Calculator-with-React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ef5ce5933080ca1d3956",
+    "url": "/Calculator-with-React/static/js/2.43c1f27d.chunk.js"
+  },
+  {
+    "revision": "2f20050d6602b2fcdccc",
+    "url": "/Calculator-with-React/static/js/main.134494ce.chunk.js"
+  },
+  {
+    "revision": "a5b8f3c6674292fb31ee",
+    "url": "/Calculator-with-React/static/js/runtime~main.2cd14f3d.js"
+  }
+]);
